d7ed9b91894d41b8bc8ab2a9e0d8e5e97b873a87
Ljubica <ljubicaa.bosanac@gmail.com>
2018-08-16 19:29:33 +0200

    Import stylesheets, add basic grid

--- app/assets/javascripts/read.js
+++ app/assets/javascripts/read.js
@@ -5,14 +5,63 @@ $(function getBooks(){
 		contentType: "application/json"
 	}).done(function(data){
 		var output = '<div>';
+    var i = 0;
 		$.each(data, function(key, data) {
-			output += '<div>';
+	
 			output += '<h3>' +data.time+'</h3>';
+      i++;
 		});
-		output += '</div>';
+
+    console.log("You've fed your cat " + i + " times");
 		$('#times').html(output);
+    $('#timesfed').html(i);
 
 	});
+
+	google.charts.load('current', {packages: ['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+      // Define the chart to be drawn.
+      var data = new google.visualization.DataTable();
+      data.addColumn('string', 'Element');
+      data.addColumn('number', 'Percentage');
+      data.addRows([
+        ['Nitrogen', 0.78],
+        ['Oxygen', 0.21],
+        ['Other', 0.01]
+      ]);
+
+      // Instantiate and draw the chart.
+      var chart = new google.visualization.PieChart(document.getElementById('myPieChart'));
+      chart.draw(data, null);
+    }
+
+    google.charts.load('current', {'packages':['bar']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Year', 'Sales', 'Expenses', 'Profit'],
+          ['2014', 1000, 400, 200],
+          ['2015', 1170, 460, 250],
+          ['2016', 660, 1120, 300],
+          ['2017', 1030, 540, 350]
+        ]);
+
+        var options = {
+          chart: {
+            title: 'Company Performance',
+            subtitle: 'Sales, Expenses, and Profit: 2014-2017',
+          }
+        };
+
+        var chart = new google.charts.Bar(document.getElementById('columnchart_material'));
+
+        chart.draw(data, google.charts.Bar.convertOptions(options));
+      }
+      
 });
+
 
 

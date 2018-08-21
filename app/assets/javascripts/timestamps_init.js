2d63522bdb657887e406463f55a94a0bdf11d86f
Ljubica <ljubicaa.bosanac@gmail.com>
2018-08-22 01:25:14 +0200

    Show daily number of feedings on chart

--- app/assets/javascripts/timestamps_init.js
+++ app/assets/javascripts/timestamps_init.js
@@ -5,12 +5,18 @@ $(function getTimestamps(){
     contentType: "application/json"
   }).done(function(data){
     var output = '<div>';
+    var i = 0;
 
     $.each(data, function(key, data) {
   
       output += '<h3>' +data.date+'</h3>';
+      i++;
     });
 
     $('#timestamps').html(output);
+    $('#timesfed').html(i);
+    
+    console.log("You've fed your cat " + i + " times");
+
   });
 });

bf1072453eb79ea6a8936b97daf968fa39e4bf8c
Ljubica <ljubicaa.bosanac@gmail.com>
2018-09-02 19:23:45 +0200

    Reverse timestamps order

--- app/assets/javascripts/timestamps_init.js
+++ app/assets/javascripts/timestamps_init.js
@@ -4,19 +4,17 @@ $(function getTimestamps(){
     type: "GET",
     contentType: "application/json"
   }).done(function(data){
-    var output = '<div>';
+
     var i = 0;
 
     $.each(data, function(key, data) {
 
- 
-  
-      output += '<tr>' + '<th>' +data.date.substring(0, data.date.length -17) +'</th>' + '<th>' +data.date.substring(11, data.date.length -11) +'</th>' +'</tr>';
+      $("#timestamps").prepend('<tr>' + '<th>' +data.date.substring(0, data.date.length -17) +'</th>' + '<th>' +data.date.substring(11, data.date.length -11) +'</th>' +'</tr>');
 
       i++;
     });
 
-    $('#timestamps').html(output);
+
     $('#timesfed').html(i);
     
     console.log("You've fed your cat " + i + " times");
